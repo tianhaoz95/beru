bb7a02f3aa0d60d6c669bbbd84c374c162b64441
Tianhao Zhou <jacksonzhou666@gmail.com>
2025-04-19 17:08:51 -0700

    feat(train_tokenizer): add CLI arguments for training parameters
    Introduce command-line arguments to customize the number of batches and batch size for tokenizer training, improving flexibility and user control

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -1,8 +1,16 @@
+import argparse
 from model.tokenizer_utils import train_tokenizer
 
 
 def main():
-    train_tokenizer()
+    parser = argparse.ArgumentParser(description='Train tokenizer with specified parameters')
+    parser.add_argument('--batches_to_train', type=int, default=100,
+                        help='Number of batches to train (default: 100)')
+    parser.add_argument('--batch_size', type=int, default=100,
+                        help='Size of each batch (default: 100)')
+    
+    args = parser.parse_args()
+    train_tokenizer(batches_to_train=args.batches_to_train, batch_size=args.batch_size)
 
 
 if __name__ == "__main__":

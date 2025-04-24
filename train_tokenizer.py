b75a4131c39a804cb729488b3281cb8a6c33e5a9
Tianhao Zhou <jacksonzhou666@gmail.com>
2025-04-23 18:32:55 -0700

    feat(train_tokenizer): add vocab_size argument and integrate BeruConfig
    This commit introduces a new argument `--vocab_size` to the tokenizer training script and integrates the `BeruConfig` class to handle configuration parameters. The changes enhance the flexibility of the tokenizer training process by allowing customization of the vocabulary size.

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -1,16 +1,38 @@
 import argparse
+
+from model.model_config import BeruConfig
 from model.tokenizer_utils import train_tokenizer
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Train tokenizer with specified parameters')
-    parser.add_argument('--batches_to_train', type=int, default=100,
-                        help='Number of batches to train (default: 100)')
-    parser.add_argument('--batch_size', type=int, default=100,
-                        help='Size of each batch (default: 100)')
-    
+    parser = argparse.ArgumentParser(
+        description="Train tokenizer with specified parameters"
+    )
+    parser.add_argument(
+        "--batches_to_train",
+        type=int,
+        default=100,
+        help="Number of batches to train (default: 100)",
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=100, help="Size of each batch (default: 100)"
+    )
+    parser.add_argument(
+        "--vocab_size",
+        type=int,
+        default=32000,
+        help="Size of vocabulary (default: 32000)",
+    )
+
     args = parser.parse_args()
-    train_tokenizer(batches_to_train=args.batches_to_train, batch_size=args.batch_size)
+
+    params = BeruConfig(vocab_size=args.vocab_size)
+
+    train_tokenizer(
+        batches_to_train=args.batches_to_train,
+        batch_size=args.batch_size,
+        params=params,
+    )
 
 
 if __name__ == "__main__":
